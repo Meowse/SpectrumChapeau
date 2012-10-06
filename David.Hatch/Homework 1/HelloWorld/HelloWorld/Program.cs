fa64e9e99fddb537ea8281ff48f4499cc73174c5
David Hatch <dshatch2@dshwebart.com>
2012-10-06 14:20:32 -0700

    David Hatch - 1st assignment

---
+++ David.Hatch/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I will recieve a grade of True if I get a grade at all, otherwise it will be false");
+            Console.ReadKey();
+        }
+    }
+}
