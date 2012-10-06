bd5259c02e370a899baaca1c21a784ec32704bfc
thomas.breigenzer <breigenzer101@gmail.com>
2012-10-06 11:31:24 -0700

    added name check and corresponding grade messages

--- thomas.breigenzer/Homework 1/HelloWorld/HelloWorld/Program.cs
+++ thomas.breigenzer/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -1,13 +1,26 @@
 ﻿using System;
-
+using System.Threading; 
 namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please rate this effort as Passing");
-            Console.ReadKey();
-        }
+            Console.WriteLine("Hello Seeker of Truth, please enter your first name");
+            //Console.WriteLine("Please rate this effort as Passing");
+            string Fname = Console.ReadLine();
+            string Cname = Fname.ToLower(); //preserving Fname in state it is entered, i.e., all caps, first char capitalized...
+            string Instructor = "mickey";
+            if (Cname.Equals(Instructor))
+            {
+                Console.WriteLine("Hello Sensei {0}! Please judge this effort to be five fingers", Fname);
+                Console.ReadLine();
+            }
+            else //(Fname.Equals("Mickey"))
+            {
+                Console.WriteLine("Hi, {0}! Sorry but discussion of grades is strictly verboten.", Fname);
+                Console.ReadLine();
+            }
+       }
     }
 }
