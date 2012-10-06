8ba848fa83139aa9c321630c2e93076128d43ee5
Sunil Pitkar <sunil_pitkar@HOTMAIL.COM>
2012-10-06 11:42:35 -0700

    Sunil Pitkar: Home work 1
    Getting homework 1 out of the way

---
+++ Sunil.Pitkar/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I survived the first class of .NET Foundations class...learnt something.");
+            Console.ReadKey();
+        }
+    }
+}
