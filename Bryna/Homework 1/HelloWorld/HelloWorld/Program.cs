42347e44506c7456800c37dd23e2cff9e565fa9f
Bulldog QA <bulldogqa@gmail.com>
2012-10-05 18:37:31 -0700

    Initial check-in. Work in progress.

---
+++ Bryna/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// Bryna Shane
+// .NET Development Foundations 
+// Homework Assignment #
+// October nn, 2012
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Change this to tell me how good a grade I should give you on this assignment.");
+            Console.ReadKey();
+           
+        }
+    }
+}
