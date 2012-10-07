afdd1fbe4f8d86774e2498aab20475c057e91359
David Hatch <dshatch2@dshwebart.com>
2012-10-06 21:40:17 -0700

    David Hatch - 1st assignment with prompt for user

--- David.Hatch/Homework 1/HelloWorld/HelloWorld/Program.cs
+++ David.Hatch/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -6,8 +6,41 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I will recieve a grade of True if I get a grade at all, otherwise it will be false");
-            Console.ReadKey();
+           
+            string str;
+            string compare_string;
+            bool result;
+
+            compare_string = "Mickey";
+           
+            Console.WriteLine("Please enter your first name");
+           
+            str = Console.ReadLine();
+
+
+           //if (str == "Mickey") // Check string this works but literal
+
+            result = string.Equals(compare_string,str,StringComparison.OrdinalIgnoreCase); // this is not case sensisive
+
+            if (result)
+
+            {
+                Console.WriteLine("Hi " + str + ", I will recieve a grade of True if I get a grade at all, otherwise it will be false!");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Hi " + str+", your name must be Mickey if you want to know what grade I think Mickey should give me.");
+                Console.ReadKey();
+            }
         }
     }
 }
+
+
+//extra credit:
+//prompt the user to enter his or her name.
+//get the user's input (try googling: "c# read console input").
+//greet the user by name (try googling: "format output in c#").
+//extra extra credit:
+//if the user's name is "Mickey", request your desired grade; if not, explain that you cannot discuss your grade with the user because he or she is not the instructor. (try googling: "compare strings in C#")
