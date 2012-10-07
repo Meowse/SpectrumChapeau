99fe807df915c2e0011e7e0854d74fd890ed4d2b
cftan <cftan@uw.edu>
2012-10-07 13:02:15 -0700

    Updated Assignment with Extra Credits

--- Leonard.Tan/Homework 1/HelloWorld/HelloWorld/Program.cs
+++ Leonard.Tan/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -6,8 +6,57 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Change this to tell me how good a grade I should give you on this assignment.");
-            Console.ReadKey();
+            
+            // 10/07/2012 Initial Leonard Tan cftan@uw.edu
+            // First program created by Abigail
+
+            //        Declaration of variables
+
+            string strName, strTime;
+            DateTime CurrTime = DateTime.Now;
+            DateTime AfternoonTime = Convert.ToDateTime("12:00:00 AM");
+
+            //        Greetings depending on time of the day
+            if (CurrTime > AfternoonTime)
+            {
+            //    Console.WriteLine("Good Morning!");
+                strTime = "Morning";
+            }
+            else
+            {
+                //Console.WriteLine("Good Afternoon!");
+                strTime = "Afternoon";
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("May I know what is your first name?\n");
+            strName = Console.ReadLine();
+            Console.WriteLine("Good {0} {1}!",strTime, strName);
+
+            
+            //          Determine Statement by first name
+
+            if (string.Equals(strName, "Mickey", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("What's the grade do you think I deserved?");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Thank you for using this program and Goodbye!");
+                Console.ReadKey();
+            }
+
+            
+//Prompt the user to enter his or her name.
+//Get the user's input (try googling: "C# read console input").
+//Greet the user by name (try googling: "format output in C#").
+
+//Extra extra credit:
+//If the user's name is "Mickey", request your desired grade; if not, explain that you cannot discuss your grade with the user because he or she is not the instructor. (try googling: "compare strings in C#")
+        
+
         }
     }
 }
