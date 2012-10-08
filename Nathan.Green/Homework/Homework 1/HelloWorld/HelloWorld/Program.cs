ed17043014e154735eabd2d95ef597c4cdb9ec9c
Nathan Green <nate@live.com>
2012-10-08 14:00:14 -0700

    Nathan Green: First check-in of my Homework1 assignment
    I have copied the Homework1 folder into my created Nathan.Green\Homework
    subfolder.  I have also updated the program to request the grade I would
    like to receive.

---
+++ Nathan.Green/Homework/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I believe I deserve a grade of A since I was able to do this on the first try!  Thanks Mickey!");
+            Console.ReadKey();
+        }
+    }
+}
