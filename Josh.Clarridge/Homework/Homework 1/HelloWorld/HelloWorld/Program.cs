c853a5cea61cf9260e226e17c939cb05812b92e9
Josh Clarridge <jsclarridge@gmail.com>
2012-10-07 19:21:40 -0700

    Josh Clarridge - turning in basic HW 1

---
+++ Josh.Clarridge/Homework/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'd like a passing grade...assuming I did this correctly.");
+            Console.ReadKey();
+        }
+    }
+}
