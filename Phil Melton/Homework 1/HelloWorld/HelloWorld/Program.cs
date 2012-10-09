45e0466ce8586df951f8f5acba08ed56411e50a7
Phil Melton <philmelton@outlook.com>
2012-10-08 19:30:01 -0700

    Phil Melton
    Hello World 1

---
+++ Phil Melton/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Feel free to give me an A. Thank you.");
+            Console.ReadKey();
+        }
+    }
+}
