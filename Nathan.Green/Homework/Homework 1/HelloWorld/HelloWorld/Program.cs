0b5c9ebd17a3463aa4aabbc5dd70eb012b609843
Nathan Green <nate@live.com>
2012-10-08 19:53:59 -0700

    Nathan Green: Added all extra credit requirements.
    I have added code to:
    1. Request the user's first name.
    2. Greets the user by first name.
    3. If the user enters Mickey it will request a passing grade, else it
    will inform the user that the grade cannot be discussed.
    
    I have also added rudimentary error checking.

--- Nathan.Green/Homework/Homework 1/HelloWorld/HelloWorld/Program.cs
+++ Nathan.Green/Homework/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,20 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I believe I deserve a grade of A since I was able to do this on the first try!  Thanks Mickey!");
+            Console.WriteLine("Please enter your first name:");
+            string userInput = Console.ReadLine();
+
+            if (userInput.Length < 1) Console.WriteLine("No name entered.");
+            else
+            {
+                Console.WriteLine("Hello {0:G}\n", userInput);
+
+                if (userInput.ToLower() == "mickey")
+                    Console.WriteLine("I believe I deserve a grade of A+ since I was able to do all the extra credit!  Thank you {0:G}\n", userInput + "!");
+                else Console.WriteLine("I am so sorry {0:G}\n", userInput + ", I cannot discuss my grade with you because you are not the instructor.");
+            }
+
+            Console.WriteLine("Please press any key to exit.");
             Console.ReadKey();
         }
     }
