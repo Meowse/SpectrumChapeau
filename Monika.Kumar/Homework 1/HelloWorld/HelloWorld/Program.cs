e945dbf395672375cc9cbf6146bc8846cd39cd76
Monika Kumar <mona.016@gmail.com>
2012-10-08 20:02:14 -0700

    Monika: Making some modifications.

--- Monika.Kumar/Homework 1/HelloWorld/HelloWorld/Program.cs
+++ Monika.Kumar/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -6,8 +6,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Change this to tell me how good a grade I should give you on this assignment.");
-            Console.ReadKey();
+            Console.WriteLine("Turning in my first homework assigment.");
+            Console.ReadLine();
         }
     }
 }
