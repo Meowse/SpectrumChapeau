1e241a49ea6c4231d039a3a9bb5b41156095d18f
Monika Kumar <mona.016@gmail.com>
2012-10-10 14:15:07 -0700

    Updated the code for extra credits

--- Monika.Kumar/Homework 1/HelloWorld/HelloWorld/Program.cs
+++ Monika.Kumar/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,20 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Turning in my first homework assigment.");
+            // Commenting and adding new code
+            //Console.WriteLine("Turning in my first homework assigment.");
+            //Console.ReadLine();
+
+            // new code for extra mile
+            string A;
+            Console.WriteLine("Enter your first name");
+            A = Console.ReadLine();
+
+            string B;
+            Console.WriteLine("Enter your Last name");
+            B = Console.ReadLine();
+
+            Console.WriteLine("User's full name is:" + " " + A + " " + B);
             Console.ReadLine();
         }
     }
