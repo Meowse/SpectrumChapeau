aac54c0d39b4bc32f3c9ed996e007f1664d0f235
Deepika Rudraraju <deepika.rudraraju@gmail.com>
2012-10-10 11:56:51 -0700

    [Deepika.Rudraraju] Submitting Assignment 1

---
+++ Deepika.Rudraraju/Homework/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Deepika, Submitting Assigment 1.");
+            Console.ReadKey();
+        }
+    }
+}
