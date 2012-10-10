0c85a8850cac729aee4b7e9a3525dde9c04d8484
Hopper <nlhopper999@hotmail.com>
2012-10-10 12:54:18 -0700

    HW1 Attempt2

---
+++ Nichole.Hopper/HOMEWORK/HW1 Attempt 2/Hello World 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hello_World_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Output text to the screen 
+            Console.WriteLine("Hello Mickey!  I think this deserves a passing grade please!");
+            Console.ReadKey();
+
+        }
+    }
+}
