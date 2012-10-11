f3df5ce79f44d3f659a58a8f1f1de1849d2ce87c
naz <savertime2@remove.gmail.remove.com>
2012-10-10 18:58:22 -0700

    Nazee.Hajebi Committing Home Work Assignment 1

---
+++ Nazee.Hajebi/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Change this to tell me how good a grade I should give you on this assignment.");
+            Console.ReadKey();
+        }
+    }
+}
