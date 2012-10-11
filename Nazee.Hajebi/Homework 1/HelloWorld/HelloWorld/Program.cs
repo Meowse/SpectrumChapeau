b9ec5daf55496fb8cd99d8af6fb71fadb3d4efd4
naz <savertime2@remove.gmail.remove.com>
2012-10-10 20:15:36 -0700

    Nazee.Hajebi  Completed assigment 1 and Extra Credit

--- Nazee.Hajebi/Homework 1/HelloWorld/HelloWorld/Program.cs
+++ Nazee.Hajebi/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -6,8 +6,27 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Change this to tell me how good a grade I should give you on this assignment.");
+            Console.WriteLine("I would like to get a passing grade on assignment 1.");
             Console.ReadKey();
+            // Extra credit
+            Console.Write(" What is your name?");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hello {0}, how are you? {0}", name);
+            Console.ReadLine();
+
+            // Extra Extra Credit
+
+            //Console.WriteLine("what is your first name?");
+            //String Firstname = Console.ReadLine();
+
+            //if (Firstname == "Mickey")
+              //  Console.WriteLine("You can request your desired grade");
+            //Console.ReadLine();
+             //else
+            
+               // Console.WriteLine("Can't discuss your grade because you are not the instructor");
+            
+           
         }
     }
 }
