b2243ca7db0fb288cc51f3b2ca7672b28e8ca633
Nathan Green <nate@live.com>
2012-10-11 19:23:57 -0700

    Nathan Green: Updated code using ReSharper
    Applied the recommendations from Resharper.

--- Nathan.Green/Homework/Homework 1/HelloWorld/HelloWorld/Program.cs
+++ Nathan.Green/Homework/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -4,21 +4,28 @@ namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Please enter your first name:");
             string userInput = Console.ReadLine();
 
-            if (userInput.Length < 1) Console.WriteLine("No name entered.");
-            else
+            if (userInput != null)
             {
-                Console.WriteLine("Hello {0:G}\n", userInput);
+                if (userInput.Length < 1) Console.WriteLine("No name entered.");
+                else
+                {
+                    Console.WriteLine("Hello {0}\n", userInput);
 
-                if (userInput.ToLower() == "mickey")
-                    Console.WriteLine("I believe I deserve a grade of A+ since I was able to do all the extra credit!  Thank you {0:G}\n", userInput + "!");
-                else Console.WriteLine("I am so sorry {0:G}\n", userInput + ", I cannot discuss my grade with you because you are not the instructor.");
+                    if (userInput.ToLower() == "mickey")
+                        Console.WriteLine(
+                            "I believe I deserve a grade of A+ since I was able to do all the extra credit!  Thank you {0}\n",
+                            userInput + "!");
+                    else
+                        Console.WriteLine("I am so sorry {0}\n",
+                                          userInput +
+                                          ", I cannot discuss my grade with you because you are not the instructor.");
+                }
             }
-
             Console.WriteLine("Please press any key to exit.");
             Console.ReadKey();
         }
