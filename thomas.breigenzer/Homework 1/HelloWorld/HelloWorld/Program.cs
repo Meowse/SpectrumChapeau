5617a3754c4690938aee8310f34e1e2f80e978ca
Meowse <meowse@remove.gmail.remove.com>
2012-10-11 20:28:14 -0700

    Mickey -- demonstrating refactoring

--- thomas.breigenzer/Homework 1/HelloWorld/HelloWorld/Program.cs
+++ thomas.breigenzer/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -1,26 +1,28 @@
 ﻿using System;
-using System.Threading; 
+
 namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+        private const string LOWER_CASE_INSTRUCTOR_NAME = "mickey";
+
+        static void Main()
         {
             Console.WriteLine("Hello Seeker of Truth, please enter your first name");
-            //Console.WriteLine("Please rate this effort as Passing");
-            string Fname = Console.ReadLine();
-            string Cname = Fname.ToLower(); //preserving Fname in state it is entered, i.e., all caps, first char capitalized...
-            string Instructor = "mickey";
-            if (Cname.Equals(Instructor))
+
+            string firstName = Console.ReadLine();
+            string lowerCaseFirstName = firstName.ToLower();
+
+            if (lowerCaseFirstName.Equals(LOWER_CASE_INSTRUCTOR_NAME))
             {
-                Console.WriteLine("Hello Sensei {0}! Please judge this effort to be five fingers", Fname);
-                Console.ReadLine();
+                Console.WriteLine("Hello Sensei {0}! Please judge this effort to be five fingers", firstName);
             }
-            else //(Fname.Equals("Mickey"))
+            else
             {
-                Console.WriteLine("Hi, {0}! Sorry but discussion of grades is strictly verboten.", Fname);
-                Console.ReadLine();
+                Console.WriteLine("Hi, {0}! Sorry but discussion of grades is strictly verboten.", firstName);
             }
-       }
+
+            Console.ReadLine();
+        }
     }
 }
