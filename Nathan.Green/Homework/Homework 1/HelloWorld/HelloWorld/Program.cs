46b908afc2c3504bb7e3b4fcc179c3de3832e965
Nathan Green <nate@live.com>
2012-10-11 20:31:49 -0700

    Nathan Green: Updated from comments in Meeting
    Just wanted to implement what I was told in class and try it out.  This
    is the final copy for fun.

--- Nathan.Green/Homework/Homework 1/HelloWorld/HelloWorld/Program.cs
+++ Nathan.Green/Homework/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -2,28 +2,32 @@
 
 namespace HelloWorld
 {
-    class Program
+    internal class Program
     {
-        static void Main()
+        private const string LOWER_CASE_INSTRUCTOR_FIRST_NAME = "mickey";
+
+        private static void Main()
         {
             Console.WriteLine("Please enter your first name:");
-            string userInput = Console.ReadLine();
+            string firstName = Console.ReadLine();
 
-            if (userInput != null)
+            if (firstName.Length == 0)
             {
-                if (userInput.Length < 1) Console.WriteLine("No name entered.");
+                Console.WriteLine("No name entered.");
+            }
+            else
+            {
+                Console.WriteLine("Hello {0}\n", firstName);
+
+                if (firstName.ToLower() == LOWER_CASE_INSTRUCTOR_FIRST_NAME)
+                {
+                    Console.WriteLine(
+                        "I believe I deserve a grade of A+ since I was able to do all the extra credit!  Thank you");
+                }
                 else
                 {
-                    Console.WriteLine("Hello {0}\n", userInput);
-
-                    if (userInput.ToLower() == "mickey")
-                        Console.WriteLine(
-                            "I believe I deserve a grade of A+ since I was able to do all the extra credit!  Thank you {0}\n",
-                            userInput + "!");
-                    else
-                        Console.WriteLine("I am so sorry {0}\n",
-                                          userInput +
-                                          ", I cannot discuss my grade with you because you are not the instructor.");
+                    Console.WriteLine(
+                        "I am so sorry I cannot discuss my grade with you because you are not the instructor.");
                 }
             }
             Console.WriteLine("Please press any key to exit.");
