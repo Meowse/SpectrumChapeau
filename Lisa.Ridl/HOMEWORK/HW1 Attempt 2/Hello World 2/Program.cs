c46d5277bbda852e19ea6df05866587c570397a4
Lisa Ridl <lisaridl@hotmail.com>
2012-10-11 19:22:58 -0700

    I had the wrong homework that is why I could not edit could - this is resolved.

---
+++ Lisa.Ridl/HOMEWORK/HW1 Attempt 2/Hello World 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hello_World_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Output text to the screen 
+            Console.WriteLine("Hello Mickey!  I finally finished my Homework, Hopefully I will not be penalized on my first assignment!");
+            Console.ReadKey();
+
+        }
+    }
+}
--- Lisa.Ridl/HOMEWORK/HW1 Attempt 2/Hello World 2/Properties/AssemblyInfo.cs
+++ Lisa.Ridl/HOMEWORK/HW1 Attempt 2/Hello World 2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Hello World, you have been so unkind to me thus far.")]
+[assembly: AssemblyTitle("Hello World 2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Hello World")]
+[assembly: AssemblyProduct("Hello World 2")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bbed5c63-152c-4949-b90d-bc3732f2a419")]
+[assembly: Guid("e0cf0ba8-8162-4cd9-8267-e2a42b75caaf")]
 
 // Version information for an assembly consists of the following four values:
 //
