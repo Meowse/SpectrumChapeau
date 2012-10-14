8584c87029e62a5083711fec000cfbc615338b96
Lisa Ridl <lisaridl@hotmail.com>
2012-10-14 07:23:24 -0700

    HomeWork2: made achange so I could sync

--- Lisa.Ridl/HOMEWORK/HW1 Attempt 2/Hello World 2/Program.cs
+++ Lisa.Ridl/HOMEWORK/HW1 Attempt 2/Hello World 2/Program.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Hello_World_2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             //Output text to the screen 
             Console.WriteLine("Hello Mickey!  I finally finished my Homework, Hopefully I will not be penalized on my first assignment!");
