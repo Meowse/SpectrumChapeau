5f57b0335828b70966a50ee347fa70b69326016e
Garima Mittal <garimamittal@live.com>
2012-10-14 21:37:13 -0700

    submitting hw1 Garima Mittal

---
+++ garima mittal/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I deserve a pass credit for attempting this homework.");
+            Console.ReadKey();
+        }
+    }
+}
