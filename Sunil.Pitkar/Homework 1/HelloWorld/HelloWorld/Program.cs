8db3e2e3c1c04b2666c30e12cbbdfc5b2659b46f
Sunil Pitkar <sunil_pitkar@HOTMAIL.COM>
2012-10-16 09:38:32 -0700

    Sunil Pitkar: Homework 1 : Extra Credit [Delayed]
    Submitting the Homework 1 for extra credit, i know its much delayed but
    wanted to comeplete it any way. Had submitted the home work assignment
    on time.

--- Sunil.Pitkar/Homework 1/HelloWorld/HelloWorld/Program.cs
+++ Sunil.Pitkar/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,19 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I survived the first class of .NET Foundations class...learnt something.");
+           // Homework 1 :  Extra credit which i has missed seeing on the class website in the week of 10/8
+            Console.WriteLine("Hello stranger, whats ur name?");
+            string name = Console.ReadLine();
+            if (name == "")
+            {
+                Console.WriteLine("Not a valid name, hit any key to exit.");
+        
+            }
+            else
+            {
+                Console.WriteLine("Hello " + name);   
+            }
+            
             Console.ReadKey();
         }
     }
