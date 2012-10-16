ca8eb0d795134a1c3bab22cbedcd40afeae75268
Monika Kumar <mona.016@gmail.com>
2012-10-16 01:40:10 -0700

    Submit DoingMathExtraCredit

--- Monika.Kumar/Homework 2/ExploringCSharp/ExploringCSharp/DoingMath.cs
+++ Monika.Kumar/Homework 2/ExploringCSharp/ExploringCSharp/DoingMath.cs
@@ -12,12 +12,13 @@ namespace ExploringCSharp
             // Notice also that ReSharper is *too* helpful, here, and keeps trying to turn "Math." into 
             // "DoingMath.".  Play with it until you figure out how to type "Math." without ReSharper
             // changing it into "DoingMath." on you.
-            return 0;
+
+            return Math.Max(number1, number2);
+
         }
 
         public int GetBiggestMagnitude(int number1, int number2)
         {
-            
             if (Math.Abs(number1) > Math.Abs(number2))
             {
                 return number1;
@@ -34,7 +35,13 @@ namespace ExploringCSharp
             // Try googling "C# exponents and logarithms".  Or just "exponents and logarithms",
             // if college math was too long ago for you (I had to look it up the last time I needed
             // to do this, so don't feel bad if you do, too).
-            return 0;
+            int i = 0;
+         
+            for(i=0;Math.Pow(10,i)<number;i++)
+            {
+            }
+
+            return Convert.ToInt32(number * Math.Pow(10, i));
         }
     }
 }
