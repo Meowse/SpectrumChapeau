ded0170876af1cf3a339186291bb284bfb3f6de3
naz <savertime2@remove.gmail.remove.com>
2012-10-17 00:31:30 -0700

    Revert "Deleted Assignment 2 to be able to baseline it"
    This reverts commit 938fdcfb829d1c95f39a9abf62e88eb1c3525dcf.

---
+++ Nazee.Hajebi/Homework 2/ExploringCSharp/ExploringCSharp/DoingMath.cs
@@ -0,0 +1,30 @@
+﻿namespace ExploringCSharp
+{
+    public class DoingMath
+    {
+        public int ReturnTheLargerNumber(int number1, int number2)
+        {
+            // Type "Math.", and look at the various mathematical functions that are defined for you.
+            // Notice that you'll have to say "using System.Math" at the top, or type "System.Math.",
+            // in order to see it.
+            // Notice also that ReSharper is *too* helpful, here, and keeps trying to turn "Math." into 
+            // "DoingMath.".  Play with it until you figure out how to type "Math." without ReSharper
+            // changing it into "DoingMath." on you.
+            return 0;
+        }
+
+        public int GetBiggestMagnitude(int number1, int number2)
+        {
+            // Try googling "C# absolute value of a number"
+            return 0;
+        }
+
+        public int MultiplyByTheNextLargerPowerOfTen(int number)
+        {
+            // Try googling "C# exponents and logarithms".  Or just "exponents and logarithms",
+            // if college math was too long ago for you (I had to look it up the last time I needed
+            // to do this, so don't feel bad if you do, too).
+            return 0;
+        }
+    }
+}
