ff799054b8ecf16c02ae5e3a349401a29b984e62
cftan <cftan@uw.edu>
2012-10-16 23:54:47 -0700

    Submit Homework 2 with Extra Credits

--- Leonard.Tan/Homework 2/ExploringCSharp/ExploringCSharp/BooleanLogic.cs
+++ Leonard.Tan/Homework 2/ExploringCSharp/ExploringCSharp/BooleanLogic.cs
@@ -17,7 +17,7 @@
         public bool NegatesItsInputSingleLine(bool input)
         {
             // Use resharper on the above to reduce it to a single line.
-            return false;
+            return !input;
         }
 
         public bool TrueIfBothInputsAreTrue(bool input1, bool input2)
@@ -48,13 +48,12 @@
 
         public bool TrueIfBothInputsAreTrueSingleLine(bool input1, bool input2)
         {
-            // Use resharper on the above to reduce it to a single line.
-            return false;
+            return input1 && input2;
         }
 
         public bool TrueIfEitherInputIsTrue(bool input1, bool input2)
         {
-            // Use resharper on this to reduce it to a single line.
+
             if (input1 == true)
             {
                 if (input2 == true)
@@ -82,7 +81,7 @@
         public bool TrueIfEitherInputIsTrueSingleLine(bool input1, bool input2)
         {
             // Use resharper on the above to reduce it to a single line.
-            return false;
+            return input1 || input2;
         }
 
         public bool MustPayExtraSurchargeToRentACar(string gender, int age)
@@ -90,7 +89,22 @@
             // Implement this one from scratch so that all tests pass.  
             // Age is a whole number.  The intended values and meanings of the string "gender"
             // can be inferred from the tests.
-            return false;
+            switch (gender)
+            {
+                case "M":
+                    if (age < 25)
+                    {
+                        return true;
+                    }
+                    return false;
+                case "F":
+                    return false;
+                case "O":
+                    return true;
+                default:
+                    return true;
+
+            }
         }
     }
 }
--- Leonard.Tan/Homework 2/ExploringCSharp/ExploringCSharp/CombiningStrings.cs
+++ Leonard.Tan/Homework 2/ExploringCSharp/ExploringCSharp/CombiningStrings.cs
@@ -12,13 +12,15 @@ namespace ExploringCSharp
         public string GreetsByCombiningStringsWithFormats(string name)
         {
             // try googling "string formatting in C#"
-            return null;
+            return string.Format("Hello, {0}", name);
         }
 
         public string GreetsByCombiningStringsWithStringBuilder(string name)
         {
-            StringBuilder builder = new StringBuilder(100);
             // Try typing "builder." and seeing what auto-complete options ReSharper gives you.
+            StringBuilder builder = new StringBuilder(100);
+            builder.Append("Hello, ");
+            builder.Append(name);
             return builder.ToString();
         }
     }
