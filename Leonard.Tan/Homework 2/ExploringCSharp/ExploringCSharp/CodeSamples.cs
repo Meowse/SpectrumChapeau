dd9728df53855e6f03689ae1472ffd377ef09246
cftan <cftan@uw.edu>
2012-10-17 00:08:38 -0700

    Submit Homework 2 with Extra Credits and CodeSamples.CS

--- Leonard.Tan/Homework 2/ExploringCSharp/ExploringCSharp/CodeSamples.cs
+++ Leonard.Tan/Homework 2/ExploringCSharp/ExploringCSharp/CodeSamples.cs
@@ -15,27 +15,40 @@ namespace ExploringCSharp
         public string SaysHello2(string name)
         {
             // try googling "string formatting in C#"
-            return null;
-        }
-
-        public string SaysHello2(string name)
-        {
-            // try Resharper
-            return null;
+            return string.Format("Hello, {0}", name);
         }
 
         public string SaysHello3(string name)
         {
-            // Extra Credit: try googling "convert byte array to string C#"
-            return null;
-        }
-
-
-
-
-        public string SaysHello2(string name)
-        {
+            // try Resharper
             return string.Format("Hello, {0}", name);
         }
+
+        public string SaysHello4    (string name)
+        {
+            // Extra Credit: try googling "convert byte array to string C#"
+            // Conde References from http://www.dotnetperls.com/convert-string-byte-array
+            byte[] array =
+            {
+                72,
+                101,
+                108,
+                108,
+                111,
+                44,
+                32,
+                76,
+                101,
+                111,
+                110,
+                97,
+                114,
+                100
+	        };
+
+            return ASCIIEncoding.ASCII.GetString(array);
+        }
+
+
     }
 }
