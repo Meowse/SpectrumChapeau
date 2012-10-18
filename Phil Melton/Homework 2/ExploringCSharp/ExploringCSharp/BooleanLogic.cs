287ff0eb987062bf396d150b9d0f9abb791e8856
Phil Melton <philmelton@outlook.com>
2012-10-17 21:23:48 -0700

    Week 2 HW Submission 1

--- Phil Melton/Homework 2/ExploringCSharp/ExploringCSharp/BooleanLogic.cs
+++ Phil Melton/Homework 2/ExploringCSharp/ExploringCSharp/BooleanLogic.cs
@@ -17,7 +17,8 @@
         public bool NegatesItsInputSingleLine(bool input)
         {
             // Use resharper on the above to reduce it to a single line.
-            return false;
+            return input != true;
+
         }
 
         public bool TrueIfBothInputsAreTrue(bool input1, bool input2)
@@ -49,7 +50,7 @@
         public bool TrueIfBothInputsAreTrueSingleLine(bool input1, bool input2)
         {
             // Use resharper on the above to reduce it to a single line.
-            return false;
+            return input1 && input2;
         }
 
         public bool TrueIfEitherInputIsTrue(bool input1, bool input2)
@@ -82,7 +83,7 @@
         public bool TrueIfEitherInputIsTrueSingleLine(bool input1, bool input2)
         {
             // Use resharper on the above to reduce it to a single line.
-            return false;
+            return input1 || input2;
         }
 
         public bool MustPayExtraSurchargeToRentACar(string gender, int age)
