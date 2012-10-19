f27abc835f4639686bc2938f8174b60dc79d71e5
Ola Folarin <ofolarin@yahoo.com>
2012-10-18 20:11:25 -0400

    HW 1

---
+++ Ola.Folarin/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For all my troubles I should get an A");
+            Console.ReadKey();
+        }
+    }
+}
