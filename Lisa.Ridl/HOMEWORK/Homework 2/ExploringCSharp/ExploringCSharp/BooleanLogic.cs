bfd61b584e512f503da60d28bfe308aaf9bb0fe9
Lisa Ridl <lisaridl@hotmail.com>
2012-10-28 14:53:38 -0700

    HW3:  Baseline

---
+++ Lisa.Ridl/HOMEWORK/Homework 2/ExploringCSharp/ExploringCSharp/BooleanLogic.cs
@@ -0,0 +1,96 @@
+﻿namespace ExploringCSharp
+{
+    public class BooleanLogic
+    {
+        public bool NegatesItsInput(bool input)
+        {
+            if (input == true)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool NegatesItsInputSingleLine(bool input)
+        {
+            // Use resharper on the above to reduce it to a single line.
+            return !input;
+        }
+
+        public bool TrueIfBothInputsAreTrue(bool input1, bool input2)
+        {
+            if (input1 == true)
+            {
+                if (input2 == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (input1 == true)
+                {
+                    return false;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool TrueIfBothInputsAreTrueSingleLine(bool input1, bool input2)
+        {
+            // Use resharper on the above to reduce it to a single line.
+            return false;
+        }
+
+        public bool TrueIfEitherInputIsTrue(bool input1, bool input2)
+        {
+            // Use resharper on this to reduce it to a single line.
+            if (input1 == true)
+            {
+                if (input2 == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                if (input2 == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool TrueIfEitherInputIsTrueSingleLine(bool input1, bool input2)
+        {
+            // Use resharper on the above to reduce it to a single line.
+            return false;
+        }
+
+        public bool MustPayExtraSurchargeToRentACar(string gender, int age)
+        {
+            // Implement this one from scratch so that all tests pass.  
+            // Age is a whole number.  The intended values and meanings of the string "gender"
+            // can be inferred from the tests.
+            return false;
+        }
+    }
+}
--- Lisa.Ridl/HOMEWORK/Homework 2/ExploringCSharp/ExploringCSharp/CodeSamples.cs
+++ Lisa.Ridl/HOMEWORK/Homework 2/ExploringCSharp/ExploringCSharp/CodeSamples.cs
@@ -1,31 +1,41 @@
-﻿using ExploringCSharp;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace ExploringCSharp
 {
     public class CodeSamples
-
-    partial class CodeSamplesImpl : CodeSamples
     {
+        public string SaysHello(string name)
+        {
+            return "Hello, " + name;
+        }
+
+        public string SaysHello2(string name)
+        {
+            // try googling "string formatting in C#"
+            return null;
+        }
+
+        public string SaysHello2(string name)
+        {
+            // try Resharper
+            return null;
+        }
+
+        public string SaysHello3(string name)
+        {
+            // Extra Credit: try googling "convert byte array to string C#"
+            return null;
+        }
+
+
+
+
+        public string SaysHello2(string name)
+        {
+            return string.Format("Hello, {0}", name);
+        }
     }
-
-    partial class CodeSamplesImpl
-    {
-    }
-
-    partial class CodeSamplesImpl : CodeSamples
-    {
-    }
-}
-
-class CodeSamplesImpl : CodeSamples
-    {
-    }
-
-internal class string2
-
-{
-}
-
-internal class String3
-{
 }
