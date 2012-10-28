5500a76383ceea5e6bd23ad7897962918554da0a
Garima Mittal <garimamittal@live.com>
2012-10-28 00:24:34 -0700

    HW3 Garima submission

---
+++ garima mittal/Homework3/WindowsFormsApplication3/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //System.Windows.Forms.TextBox
+            
+            MessageBox.Show("Hello, " + textBox1.Text);
+        }
+    }
+}
