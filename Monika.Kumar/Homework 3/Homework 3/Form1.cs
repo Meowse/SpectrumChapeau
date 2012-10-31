7d86310444b1b615c63aacf529909f3ee9be6af9
Monika Kumar <mona.016@gmail.com>
2012-10-31 00:59:50 -0700

    Submitting HW3

---
+++ Monika.Kumar/Homework 3/Homework 3/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Homework_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           string txtBox = this.textBox1.Text;
+            string message = "Hello " + txtBox;
+            MessageBox.Show(this, message, "Homework 3");
+
+        }
+    }
+}
