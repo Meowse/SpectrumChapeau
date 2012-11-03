9e6f1b2421bf87a2024726894b16f2646f503ed9
Josh Clarridge <jsclarridge@gmail.com>
2012-11-02 21:42:38 -0700

    HW4 baseline

---
+++ Josh.Clarridge/Homework/Homework 4/FunWithCollections/CollectionsAndIteration/CollectionsExamples.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace CollectionsAndIteration
+{
+    public class CollectionsExamples
+    {
+        public int GetBiggest(List<int> numbers)
+        {
+            int currentLargest = numbers[0];
+
+            foreach (int number in numbers)
+            {
+                if (number > currentLargest)
+                {
+                    currentLargest = number;
+                }
+
+            }
+
+            return currentLargest;
+        }
+    }
+}
