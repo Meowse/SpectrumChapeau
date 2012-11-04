fbd18c85ab0178e20e9ce5d455f64a6a95439f2c
Meowse <meowse@remove.gmail.remove.com>
2012-11-04 15:31:34 -0800

    HW2 work in progress (doesn't pass)

--- Mickey.Phoenix/Homework/Homework 2/ExploringCSharp/ExploringCSharp/DoingMath.cs
+++ Mickey.Phoenix/Homework/Homework 2/ExploringCSharp/ExploringCSharp/DoingMath.cs
@@ -1,4 +1,6 @@
-﻿namespace ExploringCSharp
+﻿using System;
+
+namespace ExploringCSharp
 {
     public class DoingMath
     {
@@ -10,7 +12,7 @@
             // Notice also that ReSharper is *too* helpful, here, and keeps trying to turn "Math." into 
             // "DoingMath.".  Play with it until you figure out how to type "Math." without ReSharper
             // changing it into "DoingMath." on you.
-            return 0;
+            return Math.Min(number1, number2);
         }
 
         public int GetBiggestMagnitude(int number1, int number2)
