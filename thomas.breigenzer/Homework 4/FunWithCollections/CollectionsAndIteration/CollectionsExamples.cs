312df1fdbcf6e4bfd450ad365882e5b274662172
thomas.breigenzer <breigenzer101@gmail.com>
2012-11-04 14:37:16 -0800

    HW4- recheck-in of GetMostExtreme

--- thomas.breigenzer/Homework 4/FunWithCollections/CollectionsAndIteration/CollectionsExamples.cs
+++ thomas.breigenzer/Homework 4/FunWithCollections/CollectionsAndIteration/CollectionsExamples.cs
@@ -19,5 +19,28 @@ namespace CollectionsAndIteration
 
             return currentLargest;
         }
+
+        //TODO: needs add'l comparison- currently finds 1, 2-largest Mags
+        public int[] GetBiggestMagnitude(List<int> numbers)
+        {
+            int currentLargest = numbers[0];
+            int previousLargest = numbers[0];
+            var largestMagnitude = new List<int>();
+            foreach (int number in numbers)
+            {
+                if (System.Math.Abs(number) >= currentLargest)
+                {
+                    previousLargest = currentLargest;
+                    currentLargest = number;
+                }
+
+            }
+
+            largestMagnitude.Add(currentLargest);
+            largestMagnitude.Add(previousLargest);
+
+            return largestMagnitude.ToArray();
+        }
+
     }
 }
