ab88d509c19585d3d09bc801405cbb7632470021
Pat Oceana <oceanbeing@msn.com>
2012-11-12 12:21:14 -0800

    homework 3 completed

--- pat.oceana/Homework 3/homework 3 hello world/homework 3 hello world/Form1.Designer.cs
+++ pat.oceana/Homework 3/homework 3 hello world/homework 3 hello world/Form1.Designer.cs
@@ -30,18 +30,17 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.button1.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Font = new System.Drawing.Font("Cambria", 14F, System.Drawing.FontStyle.Bold);
             this.button1.ForeColor = System.Drawing.Color.Aqua;
-            this.button1.Location = new System.Drawing.Point(171, 125);
+            this.button1.Location = new System.Drawing.Point(149, 125);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(144, 74);
-            this.button1.TabIndex = 0;
+            this.button1.Size = new System.Drawing.Size(220, 74);
+            this.button1.TabIndex = 1;
             this.button1.Text = "Hello";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.Button1Click);
@@ -51,32 +50,20 @@
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(149, 67);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(185, 30);
-            this.textBox1.TabIndex = 1;
+            this.textBox1.Size = new System.Drawing.Size(220, 30);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Please enter your name";
             this.textBox1.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(38, 21);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(453, 25);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Please enter your name and click the \'Hello\' button:";
-            this.label1.Click += new System.EventHandler(this.Label1Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(565, 376);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Homework 3 Hello World ";
-            this.Load += new System.EventHandler(this.Form1Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -86,7 +73,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
     }
 }
 
--- pat.oceana/Homework 3/homework 3 hello world/homework 3 hello world/Form1.cs
+++ pat.oceana/Homework 3/homework 3 hello world/homework 3 hello world/Form1.cs
@@ -12,25 +12,18 @@ namespace homework_3_hello_world
 
         private void Button1Click(object sender, EventArgs e)
         {
-            MessageBox.Show(textBox1 != null
+           const string initialText = "Please enter your name";
+
+            MessageBox.Show(textBox1.Text != initialText
                                 ? string.Format("Hello {0}", textBox1.Text)
                                 : string.Format(
                                     "I'm sorry I'm not allowed to talk to strangers, please enter your name in the text box and click the button again"));
-        }
-
-        private void Form1Load(object sender, EventArgs e)
-        {
-
+            textBox1.Focus();
         }
 
         private void TextBox1TextChanged(object sender, EventArgs e)
         {
-            string text = textBox1.Text;
-        }
-
-        private void Label1Click(object sender, EventArgs e)
-        {
-
+            
         }
     }
 }
