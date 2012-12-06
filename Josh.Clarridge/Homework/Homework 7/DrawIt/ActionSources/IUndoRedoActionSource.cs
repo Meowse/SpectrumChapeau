12e4fb1293e573842c67a4561a20d33d9e6d4802
Josh Clarridge <jsclarridge@gmail.com>
2012-12-05 21:18:06 -0800

    HW 7 baseline

---
+++ Josh.Clarridge/Homework/Homework 7/DrawIt/ActionSources/IUndoRedoActionSource.cs
@@ -0,0 +1,12 @@
+﻿namespace ActionSources
+{
+    public interface IUndoRedoActionSource<T> : IActionSource<T>
+    {
+        void Add(T action);
+        void Clear();
+        void Undo();
+        void Redo();
+        bool CanUndo { get; }
+        bool CanRedo { get; }
+    }
+}
---
+++ Josh.Clarridge/Homework/Homework 7/Drawing/DrawIt1.2/ActionSources/BrokenActionSource.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ActionSources
+{
+    public class BrokenActionSource<T> : IUndoRedoActionSource<T>
+    {
+        // This line says, "SimpleDrawSourceAction objects are a source of ActionsChanged events,
+        // and if you want to handle them, you should listen for them here, and you will need to be
+        // an ActionsChangedEventHandler in order to do this."  Anybody who cares about ActionsChanged
+        // events from a particular instance of SimpleDrawSourceAction will need to listen for them 
+        // by having a method with the same signature as the ActionsChangedEventHandler delegate,
+        // and by assigning that method (with "+=") to this ActionsChanged event.
+        public event ActionsChangedEventHandler ActionsChanged;
+
+        private readonly List<T> _actions = new List<T>(); 
+        private int _numberOfActionsToTake;
+
+        public IEnumerable<T> Actions {
+            get { return _actions.Take(_numberOfActionsToTake); }
+        }
+
+        public void Undo()
+        {
+            if (CanUndo)
+            {
+                _numberOfActionsToTake--;
+
+                // This line causes an ActionsChanged event to be seen by all of the delegates
+                // that are listening to the event.  Basically, it calls the method on each delegate
+                // that has been registered (by being added with "+=") as a listener on the 
+                // ActionsChanged event.
+                if (ActionsChanged != null) { ActionsChanged(); }
+            }
+        }
+
+        public void Redo()
+        {
+            if (CanRedo)
+            {
+                _numberOfActionsToTake++;
+
+                // This line also causes an ActionsChanged event to be seen by all of the delegates.
+                // Notice that we have to call ActionsChanged even though we didn't modify the actual list
+                // of actions (just how many of them should be drawn).  From the perspective of our
+                // "consumers" (the objects that are using us as a resource), our effective list of actions
+                // changes whenever _numberOfActionsToTake changes, as well as when our actual _actions
+                // list changes.  So we need to notify them that our actions have changed in either case.
+                ActionsChanged();
+            }
+        }
+
+        public bool CanUndo { get { return _numberOfActionsToTake > 0; } }
+        public bool CanRedo { get { return _numberOfActionsToTake < _actions.Count; } }
+
+        public void Add(T action)
+        {
+            _actions.Add(action);
+            _numberOfActionsToTake++;
+
+            // Notifying our listeners (our delegates) about the change in our actions.
+            if (ActionsChanged != null) { ActionsChanged(); }
+        }
+
+        public virtual void Clear()
+        {
+            _actions.Clear();
+            _numberOfActionsToTake = 0;
+
+            // Notifying our listeners (our delegates) about the change in our actions.
+            if (ActionsChanged != null) { ActionsChanged(); }
+        }
+    }
+}
