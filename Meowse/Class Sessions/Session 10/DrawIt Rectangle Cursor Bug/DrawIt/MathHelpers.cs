1b7b37726e667893436fbe75e20ce28642046d96
Mickey Phoenix <meowse@remove.gmail.remove.com>
2012-12-13 20:21:33 -0800

    Class sample 1 baseline checkin

---
+++ Meowse/Class Sessions/Session 10/DrawIt Rectangle Cursor Bug/DrawIt/MathHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+
+namespace DrawIt
+{
+    // "static", in the declaration of a class, means "You can't make instances of this class.  It only exists
+    // in order to hold some static methods."
+    public static class MathHelpers
+    {
+        // "static", in the declaration of a method, means "You don't need to make instances of this class in order
+        // to call this method.  You can just say "MathHelpers.GetRadius(...)", instead of needing to make a 
+        // "MathHelpers myMathHelpers = new MathHelpers();" and then "myMathHelpers.GetRadius(...)"
+        //
+        // It means, roughly, "This method is not part of an instance.  It belongs to the whole class."
+        public static int GetRadius(Point center, Point pointOnCircle)
+        {
+            return (int)Math.Round(Math.Sqrt(Math.Pow(center.X - pointOnCircle.X, 2) + Math.Pow(center.Y - pointOnCircle.Y,2)));
+        }
+        public static int GetRectangleSideLength(Point startPoint, Point endPoint)
+        {
+            return (int)Math.Round(Math.Sqrt(Math.Pow(startPoint.X - endPoint.X, 2) + Math.Pow(startPoint.Y - endPoint.Y, 2)));
+        }
+
+    }
+}
