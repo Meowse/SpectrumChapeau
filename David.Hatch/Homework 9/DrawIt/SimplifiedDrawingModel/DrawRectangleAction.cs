2297a61327bb30d42475f116bfee879120f2a286
David Hatch <dshatch2@dshwebart.com>
2012-12-14 18:53:57 -0800

    HW9 fix rectangle

--- David.Hatch/Homework 9/DrawIt/SimplifiedDrawingModel/DrawRectangleAction.cs
+++ David.Hatch/Homework 9/DrawIt/SimplifiedDrawingModel/DrawRectangleAction.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace SimplifiedDrawingModel
 {
@@ -8,16 +9,19 @@ namespace SimplifiedDrawingModel
         private readonly Point _center;
         private readonly Point _heightWidth;
         
-        public DrawRectangleAction(Pen pen, int x, int y, int a, int b)
+        public DrawRectangleAction(Pen pen, int x, int y, int x2, int y2)
         {
             _pen = pen;
             _center = new Point(x, y);
-            _heightWidth = new Point(a, b);
+            _heightWidth = new Point(x2, y2);
         }
 
         public void DrawOn(Graphics graphics)
-        {   
-            graphics.DrawRectangle(_pen, _center.X, _center.Y, _heightWidth.X - _center.X, _heightWidth.Y - _center.Y);
+        {
+            // this works for regular rectangle == graphics.DrawRectangle(_pen, _center.X, _center.Y, _heightWidth.X - _center.X, _heightWidth.Y - _center.Y);
+            // reverse drag with abs function on width and height - and min for starting point (frkn genius's in the .net foundation class) 
+            // no min it goes the wrong way == graphics.DrawRectangle(_pen, _center.X, _center.Y, Math.Abs(_heightWidth.X - _center.X), Math.Abs(_heightWidth.Y - _center.Y)); 
+            graphics.DrawRectangle(_pen, Math.Min(_center.X, _heightWidth.X), Math.Min(_center.Y, _heightWidth.Y), Math.Abs(_heightWidth.X - _center.X), Math.Abs(_heightWidth.Y - _center.Y));
         }
     }
 }
